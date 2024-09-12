46ff74116ac8fd5b9ea4f3f5b20003fd1633427a
ChiyembekezoYassin <chimyassin@gmail.com>
2024-09-12 23:49:31 +0200

    tikhonza izi

---
+++ test.js
@@ -0,0 +1,12 @@
+// let avg = (num1,num2,num3,num4) => {
+//     let add = (num1+num2+num3+num4)/4;
+//     console.log(add);
+// }
+// avg(2,43,2,23);
+
+// function decimal(num1,num3,num4) {
+//     let nums = (num1*num3/num4);
+//     console.log((nums));
+// }
+// decimal(432,231,88);
+
